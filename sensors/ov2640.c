fc90cb2706cf8a14dc6c28621795f17ef7d66371
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2024-04-25 18:33:10 +0200

    fixed sharpness

--- sensors/ov2640.c
+++ sensors/ov2640.c
@@ -17,7 +17,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-static uint8_t s_sharpness = 4;
+static uint8_t s_sharpness = 3;
 
 #if defined(ARDUINO_ARCH_ESP32) && defined(CONFIG_ARDUHAL_ESP_LOG)
 #include "esp32-hal-log.h"
